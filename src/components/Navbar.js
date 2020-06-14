462e26798ff0299ea4b897c2c4edf6f8616471bc
Lee Castelani <notayogilee@gmail.com>
2020-06-14 11:06:20 -0400

    initial setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo"></div>
+      <div className="links">
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
