12f204f5c7722ef6824d688bd4eadcae1040a947
Lee Castelani <notayogilee@gmail.com>
2020-06-16 14:03:59 -0400

    remove HashRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
