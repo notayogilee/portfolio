88c5cbe189e8e407a3e381b8e80f8cbed52e9456
Lee Castelani <notayogilee@gmail.com>
2020-07-17 22:45:59 -0400

    some refactor of useless classes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,50 +9,46 @@ import './Projects.scss';
 const Projects = () => {
   return (
     <section id="projects">
-      <h1 className="title">Projects</h1>
+      <h1>Projects</h1>
       <div className="container">
         <div className="project">
-          <h3 className="project-title">Green Thumb</h3>
-          <img src={greenThumb} alt="greenThumb" className="image" />
+          <h3>Green Thumb</h3>
+          <img src={greenThumb} alt="greenThumb" />
           <p>
             A gardening app co-created from scratch. Users can create, edit or remove a virtual garden with with gardening tips for their plants.  Calls made to weather API fetch current weather for the location of each garden.  React, Materialize CSS, backend by Rails & PostgresSQL.
           </p>
           <br />
-          <a href="https://github.com/notayogilee/green-thumb" target="_blank" rel="noopener noreferrer" className="github-link">View Code on GitHub</a>
+          <a href="https://github.com/notayogilee/green-thumb" target="_blank" rel="noopener noreferrer">View Code on GitHub</a>
         </div>
         <div className="project">
-          <h3 className="project-title">Jungle App</h3>
-          <img src={jungleapp} alt="jungleapp" className="image" />
-
+          <h3>Jungle App</h3>
+          <img src={jungleapp} alt="jungleapp" />
           <p>
             A mock eCommece app used to practice Ruby on Rails. We were given a variety of improvements to make in the form of 'tickets', from frontend styling to Active Records search.
           </p>
           <br />
-          <a href="https://github.com/notayogilee/jungle-rails-app" target="_blank" rel="noopener noreferrer" className="github-link">View Code on GitHub</a>
+          <a href="https://github.com/notayogilee/jungle-rails-app" target="_blank" rel="noopener noreferrer">View Code on GitHub</a>
         </div>
         <div className="project">
-          <h3 className="project-title">Scheduler</h3>
-          <img src={scheduler} alt="scheduler" className="image" />
-
+          <h3>Scheduler</h3>
+          <img src={scheduler} alt="scheduler" />
           <p>An appointment app for scheduling meetings with mentors using React (with hooks),  PostgresSQL, Axios, Classnames,  Storybook and tested with Jest and Cypress.</p>
           <br />
-          <a href="https://github.com/notayogilee/scheduler" target="_blank" rel="noopener noreferrer" className="github-link">View Code on GitHub</a>
+          <a href="https://github.com/notayogilee/scheduler" target="_blank" rel="noopener noreferrer">View Code on GitHub</a>
         </div>
         <div className="project">
-          <h3 className="project-title">Tiny App</h3>
-          <img src={tinyapp} alt="tinyapp" className="image" />
-
+          <h3>Tiny App</h3>
+          <img src={tinyapp} alt="tinyapp" />
           <p>URL shortening app inspired by TinyURL using Express and Node.js.</p>
           <br />
-          <a href="https://github.com/notayogilee/tinyapp" target="_blank" rel="noopener noreferrer" className="github-link">View Code on GitHub</a>
+          <a href="https://github.com/notayogilee/tinyapp" target="_blank" rel="noopener noreferrer">View Code on GitHub</a>
         </div>
         <div className="project">
-          <h3 className="project-title">Tweeter</h3>
-          <img src={tweeter} alt="tweeter" className="image" />
-
+          <h3>Tweeter</h3>
+          <img src={tweeter} alt="tweeter" />
           <p>A chat app copy of Twitter powered by Express, jQuery, and Node.js.</p>
           <br />
-          <a href="https://github.com/notayogilee/tweeter" target="_blank" rel="noopener noreferrer" className="github-link">View Code on GitHub</a>
+          <a href="https://github.com/notayogilee/tweeter" target="_blank" rel="noopener noreferrer">View Code on GitHub</a>
         </div>
       </div>
     </section>
